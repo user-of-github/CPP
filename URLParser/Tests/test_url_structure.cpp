f154cd3fc896cec6a1f53d6891b47326d9ed72ab
Nikita <nikitosminsk100@gmail.com>
2022-02-01 21:22:41 +0300

    Host, port and pathname - seem to be ready. Next will try to extract query and hash. No tests yet

--- URLParser/Tests/test_url_structure.cpp
+++ URLParser/Tests/test_url_structure.cpp
@@ -1,11 +1,12 @@
 struct UrlForTest
 {
-    std::string full_url;
-    std::string protocol;
-    std::string host;
+    const std::string full_url;
+    const std::string protocol;
+    const std::string host;
 };
 
 const UrlForTest tester[] = {
+        {"http://www.services.world.com/images/hardware?key1=param1&key2=param2#justsomehash", "http", "www.services.com"},
         {"http://www.services.com/images/hardware?key1=param1&key2=param2#justsomehash", "http", "www.services.com"},
         {"https://www.eurosport.ru/watch/schedule.shtml?time=12024458#somehashagain", "https", "www.eurosport.ru"},
         {"ftp.funet.fi/pub/standards/RFC/rfc959.txt#", Url::kUndefinedUrlPartDesignation, "ftp.funet.fi"},
@@ -14,9 +15,10 @@ const UrlForTest tester[] = {
         {"ftpeurosport.ru/watch/schedule.shtml?aa=bb&cc=dd#kek", Url::kUndefinedUrlPartDesignation, "ftpeurosport.ru"},
         {"fileeurosport.ru/watch/schedule.shtml?aa=bb&cc=dd#kek", Url::kUndefinedUrlPartDesignation, "fileeurosport.ru"},
         {"ftp.funet.fi/pub/standards/RFC/rfc959.txt", Url::kUndefinedUrlPartDesignation, "ftp.funet.fi"},
-        {"http://www.domain.org/a?foo=bar&bar=foo#", "http", "www.domain.org"},
+        {"http://www.domain.org/abc?foo=bar&bar=foo#", "http", "www.domain.org"},
         {"/domain.org/a?foo=bar", Url::kUndefinedUrlPartDesignation, "domain.org"},
         {"/apple.com/a?foo=bar", Url::kUndefinedUrlPartDesignation, "apple.com"},
         {"https://developer.mozilla.org/en-US/docs/Web/API/URL/pathname?q=value", "https", "developer.mozilla.org"},
-        {"https://developer.mozilla.org:443/en-US/docs/Web/API/URL/host", "https", "developer.mozilla.org:443"}
+        {"https://developer.mozilla.org:443/en-US/docs/Web/API/URL/host", "https", "developer.mozilla.org:443"},
+        {"https://developer.mozilla.org:443/en-US/docs/Web/API/URL/host?", "https", "developer.mozilla.org:443"}
 };
